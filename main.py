bf1fbe9811a925900cc968280c3bed86ff2a6e31
NERD <102736954+andi-nugroho@users.noreply.github.com>
2025-03-10 12:41:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 
-                          document=f"./{directory}/{filename}",
-                          caption=f"**File :** __{filename}__\n"
-                          f"**Size :** __{total_size} MB__\n\n"
-                          f"__Uploaded by @{BOT_URL}__",
                           file_name=f"{directory}",
                           parse_mode='md',
                           progress=progress,
