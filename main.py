7298824be774af23d565e5106cefb569d3b52063
NERD <102736954+andi-nugroho@users.noreply.github.com>
2025-03-10 12:41:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 
-                          file_name=f"{directory}",
-                          parse_mode='md',
-                          progress=progress,
-                          progress_args=(a, start, title))
-        a.delete()
         try:
             shutil.rmtree(directory)
         except:
