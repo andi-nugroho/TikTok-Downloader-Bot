dede00ad57ac52235183f15da43ee60e8557c067
NERD <102736954+andi-nugroho@users.noreply.github.com>
2025-03-10 12:40:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 
-                    if percent == 100:
-                        show = 0
-
-        a.edit(f'__Downloaded to the server!\n'
-               f'Uploading to Telegram Now ⏳__')
-        start = time.time()
-        title = filename
-        app.send_document(chat_id=message.chat.id,
                           document=f"./{directory}/{filename}",
                           caption=f"**File :** __{filename}__\n"
                           f"**Size :** __{total_size} MB__\n\n"
